b1f0e020cf332f42d5af0dc20a849b8111aa977c
phamvanthach <59685810+phamvanthach@users.noreply.github.com>
2026-06-01 20:11:55 +0800

    initial project

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+// On install — cache all app files
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('phamstore-v1').then(cache =>
+      cache.addAll(['/phamstore.html'])
+    )
+  );
+});
+
+// On fetch — serve from cache first
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
